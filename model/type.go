f70ecc0cb5cd5300e21f6c68c93f6c30daebef62
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-06-21 18:47:06 +0700

    funcation register

---
+++ model/type.go
@@ -0,0 +1,42 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	NamaLengkap     string             `bson:"namalengkap,omitempty" json:"namalengkap,omitempty"`
+	Email           string             `bson:"email,omitempty" json:"email,omitempty"`
+	Password        string             `bson:"password,omitempty" json:"password,omitempty"`
+	Confirmpassword string             `bson:"confirmpass,omitempty" json:"confirmpass,omitempty"`
+	Salt            string             `bson:"salt,omitempty" json:"salt,omitempty"`
+}
+
+type Password struct {
+	Password        string `bson:"password,omitempty" json:"password,omitempty"`
+	Newpassword     string `bson:"newpass,omitempty" json:"newpass,omitempty"`
+	Confirmpassword string `bson:"confirmpass,omitempty" json:"confirmpass,omitempty"`
+}
+
+
+type Credential struct {
+	Status  int    `json:"status" bson:"status"`
+	Token   string `json:"token,omitempty" bson:"token,omitempty"`
+	Message string `json:"message,omitempty" bson:"message,omitempty"`
+}
+
+type Response struct {
+	Status  int    `json:"status" bson:"status"`
+	Message string `json:"message,omitempty" bson:"message,omitempty"`
+}
+
+type Payload struct {
+	Id    primitive.ObjectID `json:"id"`
+	Email string             `json:"email"`
+	Exp   time.Time          `json:"exp"`
+	Iat   time.Time          `json:"iat"`
+	Nbf   time.Time          `json:"nbf"`
+}
