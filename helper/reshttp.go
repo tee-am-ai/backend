a36cfb9b23c009e66679c97130794f68ac882abf
Fatwaff <fax.mp4@gmail.com>
2024-06-24 05:50:28 +0700

    Add helper package for handling HTTP responses

---
+++ helper/reshttp.go
@@ -0,0 +1,29 @@
+package helper
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func ErrorResponse(respw http.ResponseWriter, req *http.Request, statusCode int, err, msg string) {
+	resp := map[string]string{
+		"error":   err,
+		"message": msg,
+	}
+	WriteJSON(respw, statusCode, resp)
+}
+
+func WriteJSON(respw http.ResponseWriter, statusCode int, content interface{}) {
+	respw.Header().Set("Content-Type", "application/json")
+	respw.WriteHeader(statusCode)
+	respw.Write([]byte(Jsonstr(content)))
+}
+
+func Jsonstr(strc interface{}) string {
+	jsonData, err := json.Marshal(strc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(jsonData)
+}
