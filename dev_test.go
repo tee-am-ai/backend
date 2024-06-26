eb43cf20c627ac89b20e0dd534c205c8e825fb3d
Fedhira Syaila <pedirasyaila@gmail.com>
2024-06-26 10:05:54 +0700

    commit 1

--- dev_test.go
+++ dev_test.go
@@ -4,30 +4,31 @@ import (
 	"fmt"
 	"testing"
 
+	helper "github.com/tee-am-ai/backend/helper"
 	model "github.com/tee-am-ai/backend/model"
 	module "github.com/tee-am-ai/backend/module"
 )
 
 var db = module.MongoConnect("MONGOSTRING", "team_ai")
 
-// func TestGenerateKey(t *testing.T) {
-// 	privateKey, publicKey := helper.GenerateKey()
-// 	t.Logf("PrivateKey : %v", privateKey)
-// 	t.Logf("PublicKey : %v", publicKey)
-// }
+func TestGenerateKey(t *testing.T) {
+	privateKey, publicKey := helper.GenerateKey()
+	t.Logf("PrivateKey : %v", privateKey)
+	t.Logf("PublicKey : %v", publicKey)
+}
 
-// // TestInsertOneDoc
-// func TestInsertOneDoc(t *testing.T) {
-// 	var data = map[string]interface{}{
-// 		"username": "teeamai",
-// 		"password": "12345",
-// 	}
-// 	insertedDoc, err := helper.InsertOneDoc(config.Mongoconn, "users", data)
-// 	if err != nil {
-// 		t.Errorf("Error : %v", err)
-// 	}
-// 	t.Logf("InsertedDoc : %v", insertedDoc)
-// }
+// TestInsertOneDoc
+func TestInsertOneDoc(t *testing.T) {
+	var data = map[string]interface{}{
+		"username": "teeamai",
+		"password": "12345",
+	}
+	insertedDoc, err := helper.InsertOneDoc(config.Mongoconn, "users", data)
+	if err != nil {
+		t.Errorf("Error : %v", err)
+	}
+	t.Logf("InsertedDoc : %v", insertedDoc)
+}
 
 func TestSignUp(t *testing.T) {
 	var doc model.User
