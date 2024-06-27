83e84eb1656dab65f6b7425eb7b7fe9b08e53cf4
Fatwaff <fax.mp4@gmail.com>
2024-06-27 14:09:37 +0700

    Update WriteJSON function signature

--- helper/reshttp.go
+++ helper/reshttp.go
@@ -14,13 +14,13 @@ func ErrorResponse(respw http.ResponseWriter, req *http.Request, statusCode int,
 	WriteJSON(respw, statusCode, resp)
 }
 
-func WriteJSON(respw http.ResponseWriter, statusCode int, content interface{}) {
+func WriteJSON(respw http.ResponseWriter, statusCode int, content any) {
 	respw.Header().Set("Content-Type", "application/json")
 	respw.WriteHeader(statusCode)
 	respw.Write([]byte(Jsonstr(content)))
 }
 
-func Jsonstr(strc interface{}) string {
+func Jsonstr(strc any) string {
 	jsonData, err := json.Marshal(strc)
 	if err != nil {
 		log.Fatal(err)
