b0fc2aebe2d036b75b23e5325441089369453c40
Novi <alnoviantiramadhani91@gmail.com>
2024-07-16 19:34:35 +0700

    Membuat response error Ketika validasi

--- controller/Ulasan.go
+++ controller/Ulasan.go
@@ -23,5 +23,7 @@ func AddUlasan(db *mongo.Database, col string, respw http.ResponseWriter, req *h
 
 		// Validasi input
 		if ulasan.NamaLengkap == "" || ulasan.Email == "" || ulasan.Rating == "" || ulasan.Message == "" {
+			helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
+			return
 		}
 }
