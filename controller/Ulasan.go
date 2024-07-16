98a64df7a6ce80e07ec083eb806647ba95f8f93b
Novi <alnoviantiramadhani91@gmail.com>
2024-07-16 19:52:07 +0700

    Update ulasan.go

--- controller/Ulasan.go
+++ controller/Ulasan.go
@@ -14,42 +14,41 @@ import (
 func AddUlasan(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
 	var ulasan model.Ulasan
 
-		// Decode request body menjadi struct Ulasan
-		err := json.NewDecoder(req.Body).Decode(&ulasan)
-		if err != nil {
-			helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
-			return
-		}
-
-		// Validasi input
-		if ulasan.NamaLengkap == "" || ulasan.Email == "" || ulasan.Rating == "" || ulasan.Message == "" {
-			helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
-			return
-		}
-
-			// Masukkan data ulasan ke database
-		ulasanData := bson.M{
-			"namalengkap": ulasan.NamaLengkap,
-			"email":       ulasan.Email,
-			"rating":      ulasan.Rating,
-			"message":     ulasan.Message,
-		}
-
-		insertedID, err := helper.InsertOneDoc(db, col, ulasanData)
-		if err != nil {
-			helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : insert data, "+err.Error())
-			return
-		}
-
-				// Response sukses
-		resp := map[string]any{
-			"message":    "ulasan berhasil ditambahkan",
-			"insertedID": insertedID,
-		}
-		helper.WriteJSON(respw, http.StatusCreated, resp)
+	// Decode request body menjadi struct Ulasan
+	err := json.NewDecoder(req.Body).Decode(&ulasan)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+		return
 	}
 
-	// Fungsi untuk mendapatkan semua ulasan
+	// Validasi input
+	if ulasan.NamaLengkap == "" || ulasan.Email == "" || ulasan.Rating == "" || ulasan.Message == "" {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
+		return
+	}
+
+	// Masukkan data ulasan ke database
+	ulasanData := bson.M{
+		"namalengkap": ulasan.NamaLengkap,
+		"email":       ulasan.Email,
+		"rating":      ulasan.Rating,
+		"message":     ulasan.Message,
+	}
+	insertedID, err := helper.InsertOneDoc(db, col, ulasanData)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : insert data, "+err.Error())
+		return
+	}
+
+	// Response sukses
+	resp := map[string]any{
+		"message":    "ulasan berhasil ditambahkan",
+		"insertedID": insertedID,
+	}
+	helper.WriteJSON(respw, http.StatusCreated, resp)
+}
+
+// Fungsi untuk mendapatkan semua ulasan
 func GetAllUlasan(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
 	// Ambil semua data ulasan dari database
 	ulasans, err := helper.GetAllDocs(db, col)
@@ -57,10 +56,11 @@ func GetAllUlasan(db *mongo.Database, col string, respw http.ResponseWriter, req
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : get data, "+err.Error())
 		return
 	}
-		// Response dengan data ulasan
-		resp := map[string]any{
-			"message":  "berhasil mendapatkan ulasan",
-			"ulasan": ulasans,
-		}
-		helper.WriteJSON(respw, http.StatusOK, resp)
+
+	// Response dengan data ulasan
+	resp := map[string]any{
+		"message":  "berhasil mendapatkan ulasan",
+		"ulasan": ulasans,
 	}
+	helper.WriteJSON(respw, http.StatusOK, resp)
+}
