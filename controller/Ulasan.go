0aa7abcf503863dc9ef51011ba8b8e67a7ef4d6d
Novi <alnoviantiramadhani91@gmail.com>
2024-07-16 19:31:08 +0700

    Membuat response error decode request body

--- controller/Ulasan.go
+++ controller/Ulasan.go
@@ -13,4 +13,11 @@ import (
 // Fungsi untuk menambahkan ulasan
 func AddUlasan(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
 	var ulasan model.Ulasan
+
+		// Decode request body menjadi struct Ulasan
+		err := json.NewDecoder(req.Body).Decode(&ulasan)
+		if err != nil {
+			helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+			return
+		}
 }
