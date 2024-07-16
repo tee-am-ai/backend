27c2a2244d7d729c9c2b6d28c1f2969bee2ce8e2
Novi <alnoviantiramadhani91@gmail.com>
2024-07-16 19:22:43 +0700

    Update json omitempty

--- model/type.go
+++ model/type.go
@@ -23,7 +23,7 @@ type Password struct {
 
 type Ulasan struct {
 	NamaLengkap      string `bson:"namalengkap,omitempty" json:"namalengkap,omitempty"`
-	Email					   string `bson:"email,omitempty" json:"email"`
+	Email					   string `bson:"email,omitempty" json:"email,omitempty"`
 	Rating					 string `bson:"rating,omitempty" json:"rating"`
 	Message					 string `bson:"message,omitempty" json:"message"`
 }
