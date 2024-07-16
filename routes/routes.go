c8fff30a645f2bc288e9074dee8456fd9ff072ef
Novi <alnoviantiramadhani91@gmail.com>
2024-07-16 19:50:24 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -30,6 +30,8 @@ func URL(w http.ResponseWriter, r *http.Request) {
 		controller.Chat(w, r, config.GetEnv("TOKENMODEL"))
 	case method == "POST" && path == "/add_ulasan":
 		controller.Ulasan(config.Mongoconn, "ulasan", w, r)
+	case method == "GET" && path == "/ulasan":
+		controller.Ulasan(config.Mongoconn, "ulasan", w, r)
 	default:
 		helper.ErrorResponse(w, r, http.StatusNotFound, "Not Found", "The requested resource was not found")
 	}
