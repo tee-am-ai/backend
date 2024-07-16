e911e2fec3df243c28cb34264c8ec9bc95b42b34
Novi <alnoviantiramadhani91@gmail.com>
2024-07-16 19:28:15 +0700

    Menambahkan import model

--- controller/Ulasan.go
+++ controller/Ulasan.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/tee-am-ai/backend/helper"
+	model "github.com/tee-am-ai/backend/model"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
