12116e48cd8a4f1567df97a15e9d043c253c1fa4
Novi <alnoviantiramadhani91@gmail.com>
2024-07-16 19:36:33 +0700

    Memasukkan namalengkap ke ulasandata

--- controller/Ulasan.go
+++ controller/Ulasan.go
@@ -29,6 +29,6 @@ func AddUlasan(db *mongo.Database, col string, respw http.ResponseWriter, req *h
 
 			// Masukkan data ulasan ke database
 		ulasanData := bson.M{
-	
+			"namalengkap": ulasan.NamaLengkap,
 		}
 }
