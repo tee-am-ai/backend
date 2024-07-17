633e3a95a1a6a10688de5a402852b2897861d266
dirgafebrian <fdirga63@gmail.com>
2024-07-17 18:50:31 +0700

    update origin for CORS

--- config/cors.go
+++ config/cors.go
@@ -7,11 +7,12 @@ import (
 // Daftar origins yang diizinkan
 var Origins = []string{
 	"http://localhost:8080",
+	"http://localhost:5173",
 	"http://127.0.0.1:8080",
 	"http://127.0.0.1:5500",
 	"http://127.0.0.1:5501",
 	"http://127.0.0.1:5503",
-	"http://localhost:5173",
+	"http://127.0.0.1:5173",
 	"https://tee-am-ai.github.io",
 	"https://tee-am-ai.vercel.app",
 }
