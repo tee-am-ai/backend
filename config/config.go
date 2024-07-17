a9c51347356eca5d8a66b67befcc3b1a361d47a2
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-17 17:14:52 +0700

    Update config.go to remove unused code and comments

--- config/config.go
+++ config/config.go
@@ -2,12 +2,11 @@ package config
 
 import (
 	"os"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func GetEnv(envName string) string {
-	envFile, _ := godotenv.Read("../.env")
-	return envFile[envName]
+	// envFile, _ := godotenv.Read("../.env")
+	// return envFile[envName]
 	return os.Getenv(envName)
 }
