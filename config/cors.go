5f004d196f76d7a12801a19ab530a56764951a03
dirgafebrian <fdirga63@gmail.com>
2024-07-17 18:56:36 +0700

    Update allowed origins for CORS

--- config/cors.go
+++ config/cors.go
@@ -6,8 +6,8 @@ import (
 
 // Daftar origins yang diizinkan
 var Origins = []string{
-	"http://localhost:8080",
 	"http://localhost:5173",
+	"http://localhost:8080",
 	"http://127.0.0.1:8080",
 	"http://127.0.0.1:5500",
 	"http://127.0.0.1:5501",
