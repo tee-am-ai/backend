b49c631840ffa7f268092fdff10196723e7981b5
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-17 17:34:36 +0700

    Remove commented out code for Credential struct

--- config/config.go
+++ config/config.go
@@ -1,9 +1,11 @@
 package config
 
+import "os"
+
 // "github.com/joho/godotenv"
 
-// func GetEnv(envName string) string {
-// 	// envFile, _ := godotenv.Read("../.env")
-// 	// return envFile[envName]
-// 	return os.Getenv(envName)
-// }
+func GetEnv(envName string) string {
+	// envFile, _ := godotenv.Read("../.env")
+	// return envFile[envName]
+	return os.Getenv(envName)
+}
