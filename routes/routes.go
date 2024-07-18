6cac89e5ecef87454903e6922f2b1145d8f9d857
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 22:55:18 +0700

    refactor: Update home route handler and route dependencies

--- routes/routes.go
+++ routes/routes.go
@@ -9,7 +9,7 @@ func Router() *mux.Router {
 	r := mux.NewRouter()
 	r.Use(handler.CorsMiddleware)
 
-	r.HandleFunc("/", Home).Methods("GET")
+	r.HandleFunc("/", handler.Home).Methods("GET")
 	r.HandleFunc("/signup", SignUp).Methods("POST")
 	r.HandleFunc("/login", LogIn).Methods("POST")
 	r.HandleFunc("/chat", Chat).Methods("POST")
