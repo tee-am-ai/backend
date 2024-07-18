7192599a3a32b480aae08d170dc368d38513fbdd
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-18 21:16:02 +0700

    delete comment di db

--- config/db.go
+++ config/db.go
@@ -5,17 +5,13 @@ import (
 )
 
 // MongoString adalah variabel untuk menyimpan string koneksi MongoDB yang didapatkan dari environment variable "MONGOSTRING".
-// Nilai ini diambil dari fungsi GetEnv yang diasumsikan terdapat di dalam package helper.
 var MongoString string = GetEnv("MONGOSTRING")
 
 // mongoinfo adalah variabel untuk menyimpan informasi koneksi MongoDB seperti string koneksi dan nama database.
-// Ini menggunakan struktur DBInfo yang mungkin didefinisikan di dalam package helper.
 var mongoinfo = helper.DBInfo{
 	DBString: MongoString,
 	DBName:   "db_teeamai",
 }
 
-// Mongoconn adalah variabel untuk menyimpan hasil koneksi MongoDB.
-// ErrorMongoconn akan menyimpan nilai error jika terjadi kesalahan saat melakukan koneksi.
 // Koneksi MongoDB diinisialisasi menggunakan fungsi MongoConnect yang mungkin didefinisikan di dalam package helper.
 var Mongoconn, ErrorMongoconn = helper.MongoConnect(mongoinfo)
