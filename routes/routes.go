cca774b03b69ff2aa9429fb64587fd16b956b280
dirgafebrian <fdirga63@gmail.com>
2024-07-18 20:12:31 +0700

    use handler CorsMiddleware function

--- routes/routes.go
+++ routes/routes.go
@@ -1,17 +1,13 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/gorilla/mux"
-	"github.com/tee-am-ai/backend/config"
 	"github.com/tee-am-ai/backend/handler"
-	"github.com/tee-am-ai/backend/helper"
 )
 
 func Router() *mux.Router {
 	r := mux.NewRouter()
-	r.Use(corsMiddleware)
+	r.Use(handler.CorsMiddleware)
 
 	r.HandleFunc("/", handler.Home).Methods("GET")
 	r.HandleFunc("/signup", handler.SignUp).Methods("POST")
