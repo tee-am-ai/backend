2b74269efd6dfdd709af01f218a59a80aecf6915
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 22:24:36 +0700

    feat: Add "message" field to home route response

--- handler/handler.go
+++ handler/handler.go
@@ -9,6 +9,7 @@ import (
 func Home(w http.ResponseWriter, r *http.Request) {
 	resp := map[string]string{
 		"github_repo": "https://github.com/tee-am-ai/backend",
+		"message":     "Ampun puh sepuh, aku mah masih pemula 🙏",
 	}
 	
 	helper.WriteJSON(w, http.StatusOK, resp)
