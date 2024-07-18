d2e1970b126cef274a91cd622821906d19a5af89
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-18 20:56:59 +0700

    update routes

--- routes/routes.go
+++ routes/routes.go
@@ -9,26 +9,32 @@ import (
 )
 
 func URL(w http.ResponseWriter, r *http.Request) {
+	// SetAccessControlHeaders handles CORS preflight requests. If true, return early.
 	if config.SetAccessControlHeaders(w, r) {
-		return // If it's a preflight request, return early.
+		return
 	}
 
+	// Check for MongoDB connection error. If not nil, return Internal Server Error response.
 	if config.ErrorMongoconn != nil {
 		helper.ErrorResponse(w, r, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : database, "+config.ErrorMongoconn.Error())
 		return
 	}
 
+	// Extract HTTP method and request path from the incoming request.
 	var method, path string = r.Method, r.URL.Path
+
+	// Route the request based on HTTP method and path.
 	switch {
 	case method == "GET" && path == "/":
-		Home(w, r)
+		Home(w, r) // Handle GET request to root path ("/") by calling Home function.
 	case method == "POST" && path == "/signup":
-		controller.SignUp(config.Mongoconn, "users", w, r)
+		controller.SignUp(config.Mongoconn, "users", w, r) // Handle POST request to "/signup" path.
 	case method == "POST" && path == "/login":
-		controller.LogIn(config.Mongoconn, w, r, config.GetEnv("PASETOPRIVATEKEY"))
+		controller.LogIn(config.Mongoconn, w, r, config.GetEnv("PASETOPRIVATEKEY")) // Handle POST request to "/login" path.
 	case method == "POST" && path == "/chat":
-		controller.Chat(w, r, config.GetEnv("TOKENMODEL"))
+		controller.Chat(w, r, config.GetEnv("TOKENMODEL")) // Handle POST request to "/chat" path.
 	default:
+		// If no matching route is found, return a Not Found response.
 		helper.ErrorResponse(w, r, http.StatusNotFound, "Not Found", "The requested resource was not found")
 	}
 }
