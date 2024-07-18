d0cfaf8b7b2384c7da188f8eb0e1b0334730290a
dirgafebrian <fdirga63@gmail.com>
2024-07-18 19:12:16 +0700

    add new cors

--- config/cors.go
+++ config/cors.go
@@ -6,12 +6,15 @@ import (
 
 // Daftar origins yang diizinkan
 var Origins = []string{
+	"http://localhost:5173",
 	"http://localhost:8080",
 	"http://127.0.0.1:8080",
 	"http://127.0.0.1:5500",
 	"http://127.0.0.1:5501",
 	"http://127.0.0.1:5503",
+	"http://127.0.0.1:5173",
 	"https://tee-am-ai.github.io",
+	"https://tee-am-ai.vercel.app",
 }
 
 // Fungsi untuk memeriksa apakah origin diizinkan
