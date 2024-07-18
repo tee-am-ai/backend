2d8a9e5dd2761562fb4a04b895616ea6595c0388
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-19 00:46:01 +0700

    update cors.go

--- config/cors.go
+++ config/cors.go
@@ -27,3 +27,26 @@ func isAllowedOrigin(origin string) bool {
 	return false
 }
 
+// Fungsi untuk mengatur header CORS
+func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
+	origin := r.Header.Get("Origin")
+
+	if isAllowedOrigin(origin) {
+		// Set CORS headers for the preflight request
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
+			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Max-Age", "3600")
+			w.WriteHeader(http.StatusNoContent)
+			return true
+		}
+		// Set CORS headers for the main request.
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		return false
+	}
+
+	return false
+}
