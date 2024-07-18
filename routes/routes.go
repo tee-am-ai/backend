ade33b6782a2a3cef61d4adf597b4234c19f7fbb
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 22:44:30 +0700

    feat: Update home route

--- routes/routes.go
+++ routes/routes.go
@@ -14,25 +14,32 @@ import (
 func Router() *mux.Router {
 	r := mux.NewRouter()
 	r.Use(handler.CorsMiddleware)
+
 	r.HandleFunc("/", Home).Methods("GET")
 	r.HandleFunc("/signup", SignUp).Methods("POST")
 	r.HandleFunc("/login", LogIn).Methods("POST")
 	r.HandleFunc("/chat", Chat).Methods("POST")
+
 	return r
 }
+
 func Home(w http.ResponseWriter, r *http.Request) {
 	resp := map[string]string{
 		"github_repo": "https://github.com/tee-am-ai/backend",
 		"message":     "Ampun puh sepuh, aku mah masih pemula 🙏",
 	}
+	
 	helper.WriteJSON(w, http.StatusOK, resp)
 }
+
 func SignUp(w http.ResponseWriter, r *http.Request) {
 	controller.SignUp(config.Mongoconn, "users", w, r)
 }
+
 func LogIn(w http.ResponseWriter, r *http.Request) {
 	controller.LogIn(config.Mongoconn, w, r, os.Getenv("PASETOPRIVATEKEY"))
 }
+
 func Chat(w http.ResponseWriter, r *http.Request) {
 	controller.Chat(w, r, os.Getenv("HUGGINGFACE_API_KEY"))
 }
