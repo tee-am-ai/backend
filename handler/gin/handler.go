0e145bf74d6b691cbfc500a72f87967c8c731561
dirgafebrian <fdirga63@gmail.com>
2024-07-19 01:43:26 +0700

    add Home function

--- handler/gin/handler.go
+++ handler/gin/handler.go
@@ -1,3 +1,10 @@
 package gin
 
-import ()
+import "github.com/gin-gonic/gin"
+
+func Home(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"github_repo": "https://github.com/tee-am-ai/backend",
+		"message":     "Ampun puh sepuh, aku mah masih pemula 🙏",
+	})
+}
