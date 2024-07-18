1acc8b49714e68f37fbff6b289a84f52c0ec3ecd
dirgafebrian <fdirga63@gmail.com>
2024-07-19 01:39:19 +0700

    add function main

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
 package main
 
-import ()
+import (
+	"github.com/tee-am-ai/backend/config"
+	"github.com/tee-am-ai/backend/routes/gin"
+)
+
+func main() {
+	config.LoadEnv()
+	r := gin.Router()
+
+	r.Run(":8080")
+}
