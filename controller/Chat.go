0209d4e566252017b1e6bc5fe438b29182dd63b3
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-18 18:01:07 +0700

    chore: Remove commented out code for Credential struct

--- controller/Chat.go
+++ controller/Chat.go
@@ -18,10 +18,10 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 	var chat model.AIRequest
 
 	err := json.NewDecoder(req.Body).Decode(&chat)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
-		return
-	}
+	// if err != nil {
+	// 	helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+	// 	return
+	// }
 
 	if chat.Query == "" {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
