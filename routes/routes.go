9f8cd2859282b797beede554dba6cd984951dd4c
dirgafebrian <fdirga63@gmail.com>
2024-07-18 19:19:51 +0700

    remove unused func

--- routes/routes.go
+++ routes/routes.go
@@ -8,30 +8,6 @@ import (
 	"github.com/tee-am-ai/backend/helper"
 )
 
-func URL(w http.ResponseWriter, r *http.Request) {
-	if config.SetAccessControlHeaders(w, r) {
-		return // If it's a preflight request, return early.
-	}
-
-	if config.ErrorMongoconn != nil {
-		helper.ErrorResponse(w, r, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : database, " + config.ErrorMongoconn.Error())
-		return
-	}
-
-	var method, path string = r.Method, r.URL.Path
-	switch {
-	case method == "GET" && path == "/":
-		Home(w, r)
-	case method == "POST" && path == "/signup":
-		controller.SignUp(config.Mongoconn, "users", w, r)
-	case method == "POST" && path == "/login":
-		controller.LogIn(config.Mongoconn, w, r, config.GetEnv("PASETOPRIVATEKEY"))
-	case method == "POST" && path == "/chat":
-		controller.Chat(w, r, config.GetEnv("TOKENMODEL"))
-	default:
-		helper.ErrorResponse(w, r, http.StatusNotFound, "Not Found", "The requested resource was not found")
-	}
-}
 
 func Home(respw http.ResponseWriter, req *http.Request) {
 	resp := map[string]string{
