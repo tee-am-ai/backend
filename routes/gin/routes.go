f176c57dc5813852edafbb22142f6f4d02070c93
dirgafebrian <fdirga63@gmail.com>
2024-07-19 01:46:58 +0700

    Update routes to use handler

--- routes/gin/routes.go
+++ routes/gin/routes.go
@@ -1,28 +1,17 @@
 package gin
 
 import (
-	"os"
-
 	"github.com/gin-gonic/gin"
-	"github.com/tee-am-ai/backend/config"
-	"github.com/tee-am-ai/backend/controller"
+	hgin "github.com/tee-am-ai/backend/handler/gin"
 )
 
 func Router() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/", Home)
-	r.POST("/signup", SignUp)
-	r.POST("/login", LogIn)
-	r.POST("/chat", Chat)
+	r.GET("/", hgin.Home)
+	r.POST("/signup", hgin.SignUp)
+	r.POST("/login", hgin.LogIn)
+	r.POST("/chat", hgin.Chat)
 
 	return r
 }
-
-
-
-
-
-
-
-
