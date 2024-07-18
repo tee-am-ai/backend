4cb63d1bf2e4b3ae840ab00e4f066b20351c9574
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 23:06:54 +0700

    refactor: Update CORS headers in SetAccessControlHeaders function

--- config/cors.go
+++ config/cors.go
@@ -30,6 +30,12 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 
 	if isAllowedOrigin(origin) {
 		// Set CORS headers for the preflight request
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
+			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
+			return true
+		}
 	
 	}
 
