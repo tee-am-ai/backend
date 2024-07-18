3ed73d913a774e4f3c2717bb55e2b6979952dbb0
dirgafebrian <fdirga63@gmail.com>
2024-07-18 20:19:00 +0700

    formatting mongo.go

--- helper/mongo.go
+++ helper/mongo.go
@@ -22,6 +22,7 @@ func MongoConnect(mconn DBInfo) (db *mongo.Database, err error)  {
 	if err != nil {
 		return nil, err
 	}
+
 	return client.Database(mconn.DBName), nil
 }
 
@@ -30,46 +31,57 @@ func InsertOneDoc(db *mongo.Database, col string, doc any) (insertedID primitive
 	if err != nil {
 		return
 	}
+
 	return result.InsertedID.(primitive.ObjectID), nil
 }
 
 func GetUserFromEmail(email string, db *mongo.Database) (doc model.User, err error) {
 	collection := db.Collection("users")
 	filter := bson.M{"email": email}
+
 	err = collection.FindOne(context.TODO(), filter).Decode(&doc)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
 			return doc, fmt.Errorf("email tidak ditemukan")
 		}
+
 		return doc, fmt.Errorf("kesalahan server")
 	}
+
 	return doc, nil
 }
 
 func GetAllDocs[T any](db *mongo.Database, col string, filter bson.M) (docs T, err error) {
 	ctx := context.TODO()
 	collection := db.Collection(col)
+
 	cursor, err := collection.Find(ctx, filter)
 	if err != nil {
 		return 
 	}
+
 	defer cursor.Close(ctx)
+
 	err = cursor.All(context.TODO(), &docs)
 	if err != nil {
 		return 
 	}
+
 	return 
 }
 
 func GetUserFromID(_id primitive.ObjectID, db *mongo.Database) (doc model.User, err error) {
 	collection := db.Collection("users")
 	filter := bson.M{"_id": _id}
+
 	err = collection.FindOne(context.TODO(), filter).Decode(&doc)
 	if err != nil {
 		if errors.Is(err, mongo.ErrNoDocuments) {
 			return doc, fmt.Errorf("no data found for ID %s", _id)
 		}
+
 		return doc, fmt.Errorf("error retrieving data for ID %s: %s", _id, err.Error())
 	}
+	
 	return doc, nil
 }
