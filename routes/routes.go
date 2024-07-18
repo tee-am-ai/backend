ea42944274bb4d8de1af67cc63cf78b5a5704dd2
dirgafebrian <fdirga63@gmail.com>
2024-07-18 19:21:32 +0700

    use gorilla mux

--- routes/routes.go
+++ routes/routes.go
@@ -3,16 +3,20 @@ package routes
 import (
 	"net/http"
 
-	"github.com/tee-am-ai/backend/config"
-	controller "github.com/tee-am-ai/backend/controller"
+	"github.com/gorilla/mux"
 	"github.com/tee-am-ai/backend/helper"
 )
 
+func Router() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", Home).Methods("GET")
+	return r
+}
 
 func Home(respw http.ResponseWriter, req *http.Request) {
 	resp := map[string]string{
 		"github_repo": "https://github.com/tee-am-ai/backend",
-		"message": "Ampun puh sepuh, aku mah masih pemula 🙏",
+		"message":     "Ampun puh sepuh, aku mah masih pemula 🙏",
 	}
 	helper.WriteJSON(respw, http.StatusOK, resp)
 }
