fb9aa9e50d5a7176948cb641b9a48be63f225d48
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-18 20:56:22 +0700

    update ErrorResponse function for improved error handling and code readability

--- helper/reshttp.go
+++ helper/reshttp.go
@@ -6,11 +6,21 @@ import (
 	"net/http"      // Package untuk melakukan operasi-operasi terkait HTTP seperti membuat server, mengirim permintaan, dan menerima respons
 )
 
+// ErrorResponse adalah fungsi untuk mengirim respons JSON yang berisi pesan kesalahan ke klien.
+// Fungsi ini memerlukan parameter:
+// - respw http.ResponseWriter: objek untuk menulis respons HTTP.
+// - req *http.Request: permintaan HTTP yang diterima.
+// - statusCode int: kode status HTTP yang akan dikirimkan (misalnya 400 untuk Bad Request).
+// - err string: jenis kesalahan yang terjadi.
+// - msg string: pesan terkait dengan kesalahan tersebut.
 func ErrorResponse(respw http.ResponseWriter, req *http.Request, statusCode int, err, msg string) {
+	// Menyiapkan respons JSON yang berisi informasi kesalahan
 	resp := map[string]string{
 		"error":   err,
 		"message": msg,
 	}
+
+	// Memanggil fungsi WriteJSON untuk menulis respons dalam format JSON dengan kode status yang sesuai
 	WriteJSON(respw, statusCode, resp)
 }
 
