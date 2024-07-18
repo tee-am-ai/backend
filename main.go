409dd74d654ee210a2ea0ac08ba571aef76d857d
dirgafebrian <fdirga63@gmail.com>
2024-07-18 20:26:01 +0700

    use LoadEnv function

--- main.go
+++ main.go
@@ -5,17 +5,19 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/tee-am-ai/backend/config"
 	"github.com/tee-am-ai/backend/routes"
 )
 
 func main() {
+	config.LoadEnv()
 
 	r := routes.Router()
 
 	port := ":8080"
 	fmt.Println("Server started at: http://localhost" + port)
 
-	err = http.ListenAndServe(port, r)
+	err := http.ListenAndServe(port, r)
 	if err != nil {
 		log.Fatal("Server failed to start: ", err)
 	}
