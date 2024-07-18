e258ad3ab490c55ccafd423f64553d83be517e99
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 22:24:01 +0700

    feat: Add home route handler

---
+++ handler/handler.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/tee-am-ai/backend/helper"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	resp := map[string]string{
+		"github_repo": "https://github.com/tee-am-ai/backend",
+	}
+	
+	helper.WriteJSON(w, http.StatusOK, resp)
+}
+
