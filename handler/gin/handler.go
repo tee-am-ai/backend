e04356cb6d58221fe2386533894440ac557841c0
dirgafebrian <fdirga63@gmail.com>
2024-07-19 01:44:04 +0700

    add SignUp function

--- handler/gin/handler.go
+++ handler/gin/handler.go
@@ -1,10 +1,18 @@
 package gin
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tee-am-ai/backend/config"
+	"github.com/tee-am-ai/backend/controller"
+)
 
 func Home(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"github_repo": "https://github.com/tee-am-ai/backend",
 		"message":     "Ampun puh sepuh, aku mah masih pemula 🙏",
 	})
+}
+
+func SignUp(c *gin.Context) {
+	controller.SignUp(config.Mongoconn, "users", c.Writer, c.Request)
 }
