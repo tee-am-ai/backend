21216bfd360cc155284314f7864981197df39791
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-18 20:30:29 +0700

    Remove unused code

--- config/config.go
+++ config/config.go
@@ -1,12 +1,13 @@
 package config
 
 import (
-	"os"
-	// "github.com/joho/godotenv"
+	"os" // Mengimpor paket os untuk bekerja dengan lingkungan sistem operasi
+	// "github.com/joho/godotenv" // Mengimpor paket godotenv (dikomentari)
 )
 
+// Fungsi GetEnv mengambil nilai dari environment variable berdasarkan nama yang diberikan
 func GetEnv(envName string) string {
-	// envFile, _ := godotenv.Read("../.env")
-	// return envFile[envName]
-	return os.Getenv(envName)
+	// envFile, _ := godotenv.Read("../.env") // Membaca file .env (dikomentari)
+	// return envFile[envName] // Mengembalikan nilai dari file .env berdasarkan nama variabel (dikomentari)
+	return os.Getenv(envName) // Mengembalikan nilai dari environment variable menggunakan os.Getenv
 }
