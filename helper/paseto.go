29aba4042f7db8732ac5d456e746fd2a90fee2b0
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-18 21:18:51 +0700

    Remove yg ga penting

--- helper/paseto.go
+++ helper/paseto.go
@@ -19,13 +19,29 @@ type Payload struct {
 }
 
 func Encode(id primitive.ObjectID, email, privateKey string) (string, error) {
+	// Membuat token baru menggunakan paseto.NewToken().
 	token := paseto.NewToken()
+
+	// Menetapkan waktu token diterbitkan (issued).
 	token.SetIssuedAt(time.Now())
+
+	// Menetapkan waktu token dapat digunakan (not before).
 	token.SetNotBefore(time.Now())
+
+	// Menetapkan waktu token kedaluwarsa (expired).
 	token.SetExpiration(time.Now().Add(2 * time.Hour))
-	token.Set("id", id)
-	token.SetString("email", email)
+
+	// Menetapkan nilai-nilai yang akan dimasukkan ke dalam token.
+	token.Set("id", id)             // Menetapkan nilai ID sebagai objek primitive.ObjectID.
+	token.SetString("email", email) // Menetapkan nilai email sebagai string.
+
+	// Mengonversi private key dari format hexadecimal menjadi paseto.V4AsymmetricSecretKey.
 	secretKey, err := paseto.NewV4AsymmetricSecretKeyFromHex(privateKey)
+	if err != nil {
+		return "", err // Mengembalikan error jika terjadi kesalahan dalam pembuatan secret key.
+	}
+
+	// Mengenkripsi token menggunakan secret key V4 dan mengembalikan token yang dihasilkan serta error yang terjadi.
 	return token.V4Sign(secretKey, nil), err
 }
 
