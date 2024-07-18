5698cc80c304e79e723dc7955d8d93732b79c072
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 22:37:14 +0700

    feat: Update home route handler

--- routes/routes.go
+++ routes/routes.go
@@ -1,18 +1,29 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/tee-am-ai/backend/handler"
+	"github.com/tee-am-ai/backend/helper"
 )
 
 func Router() *mux.Router {
 	r := mux.NewRouter()
 	r.Use(handler.CorsMiddleware)
 
-	r.HandleFunc("/", handler.Home).Methods("GET")
-	r.HandleFunc("/signup", handler.SignUp).Methods("POST")
-	r.HandleFunc("/login", handler.LogIn).Methods("POST")
-	r.HandleFunc("/chat", handler.Chat).Methods("POST")
+	r.HandleFunc("/", Home).Methods("GET")
+
 
 	return r
 }
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	resp := map[string]string{
+		"github_repo": "https://github.com/tee-am-ai/backend",
+		"message":     "Ampun puh sepuh, aku mah masih pemula 🙏",
+	}
+	
+	helper.WriteJSON(w, http.StatusOK, resp)
+}
+
