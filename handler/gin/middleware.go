68c63371b14a1fa63392df3867f31feafa47d6a4
dirgafebrian <fdirga63@gmail.com>
2024-07-19 01:49:07 +0700

    add CorsMiddleware function

--- handler/gin/middleware.go
+++ handler/gin/middleware.go
@@ -1,3 +1,24 @@
 package gin
 
-import ()
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tee-am-ai/backend/config"
+	"github.com/tee-am-ai/backend/helper"
+)
+
+func CorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if config.SetAccessControlHeaders(c.Writer, c.Request) {
+			return
+		}
+
+		if config.ErrorMongoconn != nil {
+			helper.ErrorResponse(c.Writer, c.Request, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : database, "+config.ErrorMongoconn.Error())
+			return
+		}
+
+		c.Next()
+	}
+}
