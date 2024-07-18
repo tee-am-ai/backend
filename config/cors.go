c8edddaa977fc97895185a583669db9e8bd8a40f
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 23:07:37 +0700

    refactor: Update SetAccessControlHeaders function

--- config/cors.go
+++ config/cors.go
@@ -34,9 +34,15 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
 			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Max-Age", "3600")
+			w.WriteHeader(http.StatusNoContent)
 			return true
 		}
-	
+		// Set CORS headers for the main request.
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		return false
 	}
 
 	return false
