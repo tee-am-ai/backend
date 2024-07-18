614e650296f00db2dbba2accad7e54f394dbc5cd
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 22:52:17 +0700

    refactor: Remove unused code

--- handler/handler.go
+++ handler/handler.go
@@ -14,18 +14,14 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		"github_repo": "https://github.com/tee-am-ai/backend",
 		"message":     "Ampun puh sepuh, aku mah masih pemula 🙏",
 	}
-	
 	helper.WriteJSON(w, http.StatusOK, resp)
 }
-
 func SignUp(w http.ResponseWriter, r *http.Request) {
 	controller.SignUp(config.Mongoconn, "users", w, r)
 }
-
 func LogIn(w http.ResponseWriter, r *http.Request) {
 	controller.LogIn(config.Mongoconn, w, r, os.Getenv("PASETOPRIVATEKEY"))
 }
-
 func Chat(w http.ResponseWriter, r *http.Request) {
 	controller.Chat(w, r, os.Getenv("HUGGINGFACE_API_KEY"))
 }
