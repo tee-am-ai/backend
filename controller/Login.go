291fa7adad7247974e7ca985f988cadc62664d97
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-18 18:02:06 +0700

    Refactor Login controller to remove commented out code

--- controller/Login.go
+++ controller/Login.go
@@ -48,14 +48,14 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : token")
 		return
 	}
-	resp := map[string]any{
-		"status":  "success",
-		"message": "login berhasil",
-		"token":   tokenstring,
-		"data" : map[string]string{
-			"email": existsDoc.Email,
-			"namalengkap": existsDoc.NamaLengkap,
-		},
-	}
+	// resp := map[string]any{
+	// 	"status":  "success",
+	// 	"message": "login berhasil",
+	// 	"token":   tokenstring,
+	// 	"data" : map[string]string{
+	// 		"email": existsDoc.Email,
+	// 		"namalengkap": existsDoc.NamaLengkap,
+	// 	},
+	// }
 	helper.WriteJSON(respw, http.StatusOK, resp)
 }
