18c7cd96e6b1376de088ecfb4418960532bb048c
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 22:36:15 +0700

    refactor: Remove unused code in handler.go

--- handler/handler.go
+++ handler/handler.go
@@ -1,31 +1,3 @@
 package handler
 
-import (
-	"net/http"
-	"os"
 
-	"github.com/tee-am-ai/backend/config"
-	"github.com/tee-am-ai/backend/controller"
-	"github.com/tee-am-ai/backend/helper"
-)
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	resp := map[string]string{
-		"github_repo": "https://github.com/tee-am-ai/backend",
-		"message":     "Ampun puh sepuh, aku mah masih pemula 🙏",
-	}
-	
-	helper.WriteJSON(w, http.StatusOK, resp)
-}
-
-func SignUp(w http.ResponseWriter, r *http.Request) {
-	controller.SignUp(config.Mongoconn, "users", w, r)
-}
-
-func LogIn(w http.ResponseWriter, r *http.Request) {
-	controller.LogIn(config.Mongoconn, w, r, os.Getenv("PASETOPRIVATEKEY"))
-}
-
-func Chat(w http.ResponseWriter, r *http.Request) {
-	controller.Chat(w, r, os.Getenv("HUGGINGFACE_API_KEY"))
-}
