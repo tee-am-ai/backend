dd65d293f2d2d3ba5bcac5564430369a0de8a2cb
dirgafebrian <fdirga63@gmail.com>
2024-07-18 19:42:30 +0700

    load environment variables with godotenv

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
 	"github.com/tee-am-ai/backend/routes"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r := routes.Router()
 	port := ":8080"
 	fmt.Println("Server started at: http://localhost" + port)
