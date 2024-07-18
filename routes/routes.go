8707b36140cec96bba5761d1e6a04009cbf13aab
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 22:16:37 +0700

    Refactor routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -2,8 +2,11 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/tee-am-ai/backend/handler"
 )
 
 func Router() *mux.Router {
-	
+	r := mux.NewRouter()
+	r.Use(handler.CorsMiddleware)
+
 }
