deabf32a7c48eddda5b3e69c17fc2702902472a9
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 22:47:42 +0700

    refactor: Remove unused code in routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,8 @@
 package routes
 
 import (
-	"net/http"
-	"os"
-
 	"github.com/gorilla/mux"
-	"github.com/tee-am-ai/backend/config"
-	"github.com/tee-am-ai/backend/controller"
 	"github.com/tee-am-ai/backend/handler"
-	"github.com/tee-am-ai/backend/helper"
 )
 
 func Router() *mux.Router {
@@ -21,25 +15,4 @@ func Router() *mux.Router {
 	r.HandleFunc("/chat", Chat).Methods("POST")
 
 	return r
-}
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	resp := map[string]string{
-		"github_repo": "https://github.com/tee-am-ai/backend",
-		"message":     "Ampun puh sepuh, aku mah masih pemula 🙏",
-	}
-	
-	helper.WriteJSON(w, http.StatusOK, resp)
-}
-
-func SignUp(w http.ResponseWriter, r *http.Request) {
-	controller.SignUp(config.Mongoconn, "users", w, r)
-}
-
-func LogIn(w http.ResponseWriter, r *http.Request) {
-	controller.LogIn(config.Mongoconn, w, r, os.Getenv("PASETOPRIVATEKEY"))
-}
-
-func Chat(w http.ResponseWriter, r *http.Request) {
-	controller.Chat(w, r, os.Getenv("HUGGINGFACE_API_KEY"))
 }
