71e4a9ca24e72794b73d962900c6d1826b75a764
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-19 11:08:39 +0700

    handle non-200 status code in Chat controller

--- controller/Chat.go
+++ controller/Chat.go
@@ -90,14 +90,14 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 		return
 	}
 
-	// if len(data) > 0 {
-	// 	generatedText, ok := data[0]["generated_text"].(string)
-	// 	if !ok {
-	// 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
-	// 		return
-	// 	}
-	// 	helper.WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText})
-	// } else {
-	// 	helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
-	// }
+	if len(data) > 0 {
+		generatedText, ok := data[0]["generated_text"].(string)
+		if !ok {
+			helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error extracting generated text")
+			return
+		}
+		helper.WriteJSON(respw, http.StatusOK, map[string]string{"answer": generatedText})
+	} else {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server: response")
+	}
 }
