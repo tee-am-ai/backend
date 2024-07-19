216b084bf8c34114ffc57646599c840fb2c1d42a
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-19 11:27:36 +0700

    Refactor config.go to remove commented code and update GetEnv function

--- config/config.go
+++ config/config.go
@@ -4,13 +4,6 @@ import (
 	"os" // Mengimpor paket os untuk bekerja dengan lingkungan sistem operasi
 )
 
-// Fungsi GetEnv mengambil nilai dari environment variable berdasarkan nama yang diberikan
-// func GetEnv(envName string) string {
-// 	return os.Getenv(envName) // Mengembalikan nilai dari environment variable menggunakan os.Getenv
-// }
-
 func GetEnv(envName string) string {
-	// envFile, _ := godotenv.Read("../.env")
-	// return envFile[envName]
 	return os.Getenv(envName)
 }
