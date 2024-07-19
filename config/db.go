c1cf1e1d62ab84110eb8be67bdb50c7aaa96a104
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-19 11:45:35 +0700

    updte db.go

--- config/db.go
+++ config/db.go
@@ -4,14 +4,10 @@ import (
 	"github.com/tee-am-ai/backend/helper" // Package yang mungkin berisi fungsi-fungsi bantuan terkait konfigurasi atau pengaturan
 )
 
-// MongoString adalah variabel untuk menyimpan string koneksi MongoDB yang didapatkan dari environment variable "MONGOSTRING".
 var MongoString string = GetEnv("MONGOSTRING")
 
-// mongoinfo adalah variabel untuk menyimpan informasi koneksi MongoDB seperti string koneksi dan nama database.
 var mongoinfo = helper.DBInfo{
 	DBString: MongoString,
 	DBName:   "db_teamai",
 }
-
-// Koneksi MongoDB diinisialisasi menggunakan fungsi MongoConnect yang mungkin didefinisikan di dalam package helper.
 var Mongoconn, ErrorMongoconn = helper.MongoConnect(mongoinfo)
