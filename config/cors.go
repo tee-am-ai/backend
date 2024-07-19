98ce47d997c27c97331af6e0e1faea4e797e232b
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-19 10:57:13 +0700

    Update CORS configuration

--- config/cors.go
+++ config/cors.go
@@ -8,10 +8,10 @@ import (
 var Origins = []string{
 	"http://localhost:8080",
 	"http://127.0.0.1:8080",
-	"http://127.0.0.1:5500",
-	"http://127.0.0.1:5501",
-	"http://127.0.0.1:5503",
-	"https://tee-am-ai.github.io",
+	// "http://127.0.0.1:5500",
+	// "http://127.0.0.1:5501",
+	// "http://127.0.0.1:5503",
+	// "https://tee-am-ai.github.io",
 }
 
 // Fungsi untuk memeriksa apakah origin diizinkan
