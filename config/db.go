5f14c4eb3cdf88a1e2389a7c66c0ba07402f1efb
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-19 15:19:19 +0700

    refactor: Remove commented out code and unused imports in multiple files

--- config/db.go
+++ config/db.go
@@ -1,14 +1,14 @@
-package config
+// package config
 
-import (
-	"github.com/tee-am-ai/backend/helper"
-)
+// import (
+// 	"github.com/tee-am-ai/backend/helper"
+// )
 
-var MongoString string = GetEnv("MONGOSTRING")
+// var MongoString string = GetEnv("MONGOSTRING")
 
-var mongoinfo = helper.DBInfo{
-	DBString: MongoString,
-	DBName:   "db_teeamai",
-}
+// var mongoinfo = helper.DBInfo{
+// 	DBString: MongoString,
+// 	DBName:   "db_teeamai",
+// }
 
-var Mongoconn, ErrorMongoconn = helper.MongoConnect(mongoinfo)
+// var Mongoconn, ErrorMongoconn = helper.MongoConnect(mongoinfo)
