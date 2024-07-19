aaad08dd3bc1be855f414de0179ef34a40cec6bf
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-19 15:12:36 +0700

    refactor: Update CORS headers for better security and flexibility

--- config/cors.go
+++ config/cors.go
@@ -33,15 +33,15 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 
 	if isAllowedOrigin(origin) {
 		// Set CORS headers for the preflight request
-		// if r.Method == http.MethodOptions {
-		// 	w.Header().Set("Access-Control-Allow-Credentials", "true")
-		// 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
-		// 	w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
-		// 	w.Header().Set("Access-Control-Allow-Origin", origin)
-		// 	w.Header().Set("Access-Control-Max-Age", "3600")
-		// 	w.WriteHeader(http.StatusNoContent)
-		// 	return true
-		// }
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
+			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Max-Age", "3600")
+			w.WriteHeader(http.StatusNoContent)
+			return true
+		}
 		// Set CORS headers for the main request.
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Origin", origin)
