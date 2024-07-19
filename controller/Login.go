fe44ed036d09e16c34aa7ff93b52e27c1d75da0f
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-19 11:44:39 +0700

    Refactor Login.go to remove unnecessary code and improve code organization

--- controller/Login.go
+++ controller/Login.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"encoding/hex"
 	"encoding/json"
 	"net/http"
 
@@ -9,7 +8,6 @@ import (
 	"github.com/tee-am-ai/backend/helper"
 	model "github.com/tee-am-ai/backend/model"
 	"go.mongodb.org/mongo-driver/mongo"
-	"golang.org/x/crypto/argon2"
 )
 
 // user
@@ -33,16 +31,7 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : get email " + err.Error())
 		return
 	}
-	salt, err := hex.DecodeString(existsDoc.Salt)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
-		return
-	}
-	hash := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
-	if hex.EncodeToString(hash) != existsDoc.Password {
-		helper.ErrorResponse(respw, req, http.StatusUnauthorized, "Unauthorized", "password salah")
-		return
-	}
+	
 	tokenstring, err := helper.Encode(user.ID, user.Email, privatekey)
 	if err != nil {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : token")
