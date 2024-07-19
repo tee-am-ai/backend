7cc98afa6038d1116f1d3ab51b96c9a1f11bb305
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-19 11:44:46 +0700

    Refactor GetEnv function for improved code organization and readability

--- config/config.go
+++ config/config.go
@@ -4,6 +4,8 @@ import (
 	"os" // Mengimpor paket os untuk bekerja dengan lingkungan sistem operasi
 )
 
+// GetEnv adalah fungsi yang mengambil nilai dari variabel lingkungan berdasarkan nama yang diberikan.
 func GetEnv(envName string) string {
+	// Mengambil nilai dari variabel lingkungan dengan nama envName menggunakan os.Getenv.
 	return os.Getenv(envName)
 }
