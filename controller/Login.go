8150280b68e80a8b432d4154af667c72d5557a76
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-19 11:38:59 +0700

    update login.go checkmail validate

--- controller/Login.go
+++ controller/Login.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/badoux/checkmail"
 	"github.com/tee-am-ai/backend/helper"
 	model "github.com/tee-am-ai/backend/model"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -23,10 +24,10 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
 		return
 	}
-	// if err = checkmail.ValidateFormat(user.Email); err != nil {
-	// 	helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
-	// 	return
-	// }
+	if err = checkmail.ValidateFormat(user.Email); err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
+		return
+	}
 	existsDoc, err := helper.GetUserFromEmail(user.Email, db)
 	if err != nil {
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : get email " + err.Error())
