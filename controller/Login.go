217fcbb5aa867933e7682a71b944ac00d64b9c1f
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-19 11:46:24 +0700

    update login

--- controller/Login.go
+++ controller/Login.go
@@ -13,8 +13,12 @@ import (
 )
 
 // LogIn adalah fungsi untuk menangani permintaan login pengguna.
+// LogIn adalah handler fungsi untuk menangani permintaan login.
 func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, privatekey string) {
+	// Mendeklarasikan variabel user sebagai tipe model.User
 	var user model.User
+
+	// Mendekode body permintaan JSON ke dalam variabel user
 	err := json.NewDecoder(req.Body).Decode(&user)
 	if err != nil {
 		// Jika terjadi kesalahan dalam parsing data, kirim respons dengan status Bad Request dan pesan kesalahan
@@ -24,12 +28,14 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 
 	// Memastikan bahwa email dan password tidak kosong
 	if user.Email == "" || user.Password == "" {
+		// Jika email atau password kosong, kirim respons dengan status Bad Request dan pesan kesalahan
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
 		return
 	}
 
 	// Validasi format email menggunakan package checkmail
 	if err = checkmail.ValidateFormat(user.Email); err != nil {
+		// Jika format email tidak valid, kirim respons dengan status Bad Request dan pesan kesalahan
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
 		return
 	}
@@ -37,6 +43,7 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 	// Mendapatkan data pengguna berdasarkan email dari database
 	existsDoc, err := helper.GetUserFromEmail(user.Email, db)
 	if err != nil {
+		// Jika terjadi kesalahan saat mendapatkan email dari database, kirim respons dengan status Internal Server Error dan pesan kesalahan
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : get email "+err.Error())
 		return
 	}
@@ -44,13 +51,17 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 	// Mendekode salt dari string heksadesimal menjadi slice byte
 	salt, err := hex.DecodeString(existsDoc.Salt)
 	if err != nil {
+		// Jika terjadi kesalahan saat mendekode salt, kirim respons dengan status Internal Server Error dan pesan kesalahan
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
 		return
 	}
+
+	// Menghasilkan hash dari password yang diberikan oleh pengguna menggunakan algoritma Argon2
 	hash := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
 
 	// Membandingkan hasil hash dengan hash yang tersimpan di database
 	if hex.EncodeToString(hash) != existsDoc.Password {
+		// Jika password tidak cocok, kirim respons dengan status Unauthorized dan pesan kesalahan
 		helper.ErrorResponse(respw, req, http.StatusUnauthorized, "Unauthorized", "password salah")
 		return
 	}
@@ -58,9 +69,12 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 	// Jika autentikasi berhasil, menghasilkan token menggunakan helper.Encode
 	tokenstring, err := helper.Encode(user.ID, user.Email, privatekey)
 	if err != nil {
+		// Jika terjadi kesalahan saat menghasilkan token, kirim respons dengan status Internal Server Error dan pesan kesalahan
 		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : token")
 		return
 	}
+
+	// Menyusun respons sukses dengan token yang dihasilkan dan data pengguna
 	resp := map[string]interface{}{
 		"status":  "success",
 		"message": "login berhasil",
@@ -70,5 +84,7 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 			"namalengkap": existsDoc.NamaLengkap,
 		},
 	}
+
+	// Mengirimkan respons dalam format JSON dengan status OK
 	helper.WriteJSON(respw, http.StatusOK, resp)
 }
