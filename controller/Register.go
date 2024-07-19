ef4eda8a9ed0336745b9eee73ec32f610bf5a0d0
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-19 15:12:55 +0700

    refactor: Remove password length validation and salt generation in SignUp function

--- controller/Register.go
+++ controller/Register.go
@@ -41,16 +41,16 @@ func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "password tidak boleh mengandung spasi")
 		return
 	}
-	// if len(user.Password) < 8 {
-	// 	helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "password minimal 8 karakter")
-	// 	return
-	// }
-	// salt := make([]byte, 16)
-	// _, err = rand.Read(salt)
-	// if err != nil {
-	// 	helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
-	// 	return
-	// }
+	if len(user.Password) < 8 {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "password minimal 8 karakter")
+		return
+	}
+	salt := make([]byte, 16)
+	_, err = rand.Read(salt)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
+		return
+	}
 	hashedPassword := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
 	userData := bson.M{
 		"namalengkap": user.NamaLengkap,
