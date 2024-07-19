06a55cc6657def6111654fc5622b1c5ecb69629c
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-19 11:41:42 +0700

    Update SignUp function to remove unnecessary data validation

--- controller/Register.go
+++ controller/Register.go
@@ -24,10 +24,7 @@ func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http
 		return
 	}
 
-	if user.NamaLengkap == "" || user.Email == "" || user.Password == "" || user.Confirmpassword == "" {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
-		return
-	}
+
 	if err := checkmail.ValidateFormat(user.Email); err != nil {
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email anda tidak valid")
 		return
