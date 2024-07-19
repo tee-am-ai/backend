928a68ac67c86b3ec48d1729980b2e8648c7b1f5
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-19 15:19:49 +0700

    refactor: Remove commented out code and unused imports in multiple files

--- helper/mongo.go
+++ helper/mongo.go
@@ -12,10 +12,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// type DBInfo struct {
-// 	DBString string
-// 	DBName   string
-// }
+type DBInfo struct {
+	DBString string
+	DBName   string
+}
 
 func MongoConnect(mconn DBInfo) (db *mongo.Database, err error)  {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mconn.DBString))
