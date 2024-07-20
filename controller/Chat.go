b000e06b42954fce2131d3084c67891c94b53e1d
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-20 16:05:45 +0700

    refactor: Remove commented out code and unused imports in Chat.go

--- controller/Chat.go
+++ controller/Chat.go
@@ -30,21 +30,21 @@ func Chat(respw http.ResponseWriter, req *http.Request, tokenmodel string) {
 
 	client := resty.New()
 
-	// Hugging Face API URL dan token
-	// apiUrl := config.GetEnv("HUGGINGFACE_API_KEY")
-	// apiToken := "Bearer " + tokenmodel
+	Hugging Face API URL dan token
+	apiUrl := config.GetEnv("HUGGINGFACE_API_KEY")
+	apiToken := "Bearer " + tokenmodel
 
-	// var response *resty.Response
-	// var retryCount int
-	// maxRetries := 5
-	// retryDelay := 20 * time.Second
+	var response *resty.Response
+	var retryCount int
+	maxRetries := 5
+	retryDelay := 20 * time.Second
 	
-	// parsedURL, err := url.Parse(apiUrl)
+	parsedURL, err := url.Parse(apiUrl)
 
-	// if err != nil {
-	// 	helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing URL model hugging face"+err.Error())
-    //     return
-    // }
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "error parsing URL model hugging face"+err.Error())
+        return
+    }
 
 	segments := strings.Split(parsedURL.Path, "/")
 
