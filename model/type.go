83ac4eb4baa1691965c7318f96e98ddf74391f2d
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-20 17:08:09 +0700

    update struct

--- model/type.go
+++ model/type.go
@@ -6,14 +6,14 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type User struct {
-	ID              primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-	NamaLengkap     string             `bson:"namalengkap,omitempty" json:"namalengkap,omitempty"`
-	Email           string             `bson:"email,omitempty" json:"email,omitempty"`
-	Password        string             `bson:"password,omitempty" json:"password,omitempty"`
-	Confirmpassword string             `bson:"confirmpass,omitempty" json:"confirmpass,omitempty"`
-	Salt            string             `bson:"salt,omitempty" json:"salt,omitempty"`
-}
+// type User struct {
+// 	ID              primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+// 	NamaLengkap     string             `bson:"namalengkap,omitempty" json:"namalengkap,omitempty"`
+// 	Email           string             `bson:"email,omitempty" json:"email,omitempty"`
+// 	Password        string             `bson:"password,omitempty" json:"password,omitempty"`
+// 	Confirmpassword string             `bson:"confirmpass,omitempty" json:"confirmpass,omitempty"`
+// 	Salt            string             `bson:"salt,omitempty" json:"salt,omitempty"`
+// }
 
 type Password struct {
 	Password        string `bson:"password,omitempty" json:"password,omitempty"`
