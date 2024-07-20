88dd6c827fc443f248448aa2634879e8a90146c7
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-20 16:58:21 +0700

    Add support for loading environment variables from .env file

--- config/config.go
+++ config/config.go
@@ -1,11 +1,14 @@
 package config
 
 import (
-	"os" // Mengimpor paket os untuk bekerja dengan lingkungan sistem operasi
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
-// GetEnv adalah fungsi yang mengambil nilai dari variabel lingkungan berdasarkan nama yang diberikan.
-func GetEnv(envName string) string {
-	// Mengambil nilai dari variabel lingkungan dengan nama envName menggunakan os.Getenv.
-	return os.Getenv(envName)
+func LoadEnv(path ...string) {
+	err := godotenv.Load(path...)
+	if err != nil {
+		log.Fatalf("Error loading .env file %v", err)
+	}
 }
