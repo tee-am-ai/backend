7d5b32ff1ce8067f233446c0584ae4e56dff8625
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-20 17:00:29 +0700

    Refactor WriteJSON function

--- helper/reshttp.go
+++ helper/reshttp.go
@@ -1,41 +1,31 @@
 package helper
 
 import (
-	"encoding/json" // Package untuk encoding dan decoding data dalam format JSON
-	"log"           // Package untuk logging, digunakan untuk mencatat informasi atau kesalahan ke konsol atau file log
-	"net/http"      // Package untuk melakukan operasi-operasi terkait HTTP seperti membuat server, mengirim permintaan, dan menerima respons
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
-// func ErrorResponse(respw http.ResponseWriter, req *http.Request, statusCode int, err, msg string) {
-// 	// Menyiapkan respons JSON yang berisi informasi kesalahan
-// 	resp := map[string]string{
-// 		"error":   err,
-// 		"message": msg,
-// 	}
+func ErrorResponse(respw http.ResponseWriter, req *http.Request, statusCode int, err, msg string) {
+	resp := map[string]string{
+		"error":   err,
+		"message": msg,
+	}
 
-// 	// Memanggil fungsi WriteJSON untuk menulis respons dalam format JSON dengan kode status yang sesuai
-// 	WriteJSON(respw, statusCode, resp)
-// }
-
-func WriteJSON(respw http.ResponseWriter, statusCode int, content any) {
-	// Mengatur header untuk menetapkan jenis konten sebagai application/json.
-	respw.Header().Set("Content-Type", "application/json")
-
-	// Menetapkan status kode HTTP untuk respons.
-	respw.WriteHeader(statusCode)
-
-	// Mengubah konten ke dalam bentuk string JSON menggunakan fungsi Jsonstr.
-	jsonContent := Jsonstr(content)
-
-	// Menulis respons dalam bentuk byte array ke http.ResponseWriter.
-	respw.Write([]byte(jsonContent))
+	WriteJSON(respw, statusCode, resp)
 }
 
-// Jsonstr converts any structure (`strc any`) into a JSON string representation.
+func WriteJSON(respw http.ResponseWriter, statusCode int, content any) {
+	respw.Header().Set("Content-Type", "application/json")
+	respw.WriteHeader(statusCode)
+	respw.Write([]byte(Jsonstr(content)))
+}
+
 func Jsonstr(strc any) string {
 	jsonData, err := json.Marshal(strc)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	return string(jsonData)
 }
