a4b08ce09efda0760ff17f02ece94e75b258e363
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-20 16:07:28 +0700

    refactor: Remove commented out code and unused imports in Login.go

--- controller/Login.go
+++ controller/Login.go
@@ -17,15 +17,15 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 	var user model.User
 	err := json.NewDecoder(req.Body).Decode(&user)
 	if err != nil {
-		// helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body " + err.Error())
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body " + err.Error())
 		return
 	}
 	if user.Email == "" || user.Password == "" {
-		// helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
 		return
 	}
 	if err = checkmail.ValidateFormat(user.Email); err != nil {
-		// helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
 		return
 	}
 	existsDoc, err := helper.GetUserFromEmail(user.Email, db)
@@ -35,12 +35,12 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 	// }
 	salt, err := hex.DecodeString(existsDoc.Salt)
 	if err != nil {
-		// helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
 		return
 	}
 	hash := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
 	if hex.EncodeToString(hash) != existsDoc.Password {
-		// helper.ErrorResponse(respw, req, http.StatusUnauthorized, "Unauthorized", "password salah")
+		helper.ErrorResponse(respw, req, http.StatusUnauthorized, "Unauthorized", "password salah")
 		return
 	}
 	tokenstring, err := helper.Encode(user.ID, user.Email, privatekey)
