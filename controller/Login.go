e70dcf65f3b83eb75ef7f4edbfc391e0587a4812
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-20 18:42:24 +0700

    Refactor LogIn function in Login controller and update parameter names

--- controller/Login.go
+++ controller/Login.go
@@ -12,46 +12,46 @@ import (
 	"golang.org/x/crypto/argon2"
 )
 
-func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, privatekey string) {
+func LogIn(db *mongo.Database, w http.ResponseWriter, r *http.Request, col, privatekey string) {
 	var user model.User
 
-	err := json.NewDecoder(req.Body).Decode(&user)
+	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body " + err.Error())
+		helper.ErrorResponse(w, r, http.StatusBadRequest, "Bad Request", "error parsing request body " + err.Error())
 		return
 	}
 
 	if user.Email == "" || user.Password == "" {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
+		helper.ErrorResponse(w, r, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data")
 		return
 	}
 
 	if err = checkmail.ValidateFormat(user.Email); err != nil {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
+		helper.ErrorResponse(w, r, http.StatusBadRequest, "Bad Request", "email tidak valid")
 		return
 	}
 
-	existsDoc, err := helper.GetUserFromEmail(user.Email, db)
+	existsDoc, err := helper.GetUserFromEmail(db, col, user.Email)
 	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : get email " + err.Error())
+		helper.ErrorResponse(w, r, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : get email " + err.Error())
 		return
 	}
 
 	salt, err := hex.DecodeString(existsDoc.Salt)
 	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
+		helper.ErrorResponse(w, r, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
 		return
 	}
 
 	hash := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
 	if hex.EncodeToString(hash) != existsDoc.Password {
-		helper.ErrorResponse(respw, req, http.StatusUnauthorized, "Unauthorized", "password salah")
+		helper.ErrorResponse(w, r, http.StatusUnauthorized, "Unauthorized", "password salah")
 		return
 	}
 
 	tokenstring, err := helper.Encode(user.ID, user.Email, privatekey)
 	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : token")
+		helper.ErrorResponse(w, r, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : token")
 		return
 	}
 
@@ -65,5 +65,5 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 		},
 	}
 
-	helper.WriteJSON(respw, http.StatusOK, resp)
+	helper.WriteJSON(w, http.StatusOK, resp)
 }
