e3f13f3ee92d4877af42b242ab0bc1de669afbe7
Fatwaff <fax.mp4@gmail.com>
2024-07-21 09:27:38 +0700

    Add JSON decoding for Ulasan struct

--- controller/Ulasan.go
+++ controller/Ulasan.go
@@ -1,8 +1,10 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/tee-am-ai/backend/helper"
 	model "github.com/tee-am-ai/backend/model"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -11,7 +13,12 @@ import (
 func AddUlasan(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
 	var ulasan model.Ulasan
 
-	
+	// Decode request body menjadi struct Ulasan
+	err := json.NewDecoder(req.Body).Decode(&ulasan)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+		return
+	}
 	
 }
 
