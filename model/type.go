6dfde04560cedd5004252b9e2f94012a4cf65239
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-21 21:45:39 +0700

    update imports in type.go

--- model/type.go
+++ model/type.go
@@ -10,16 +10,16 @@ type User struct {
 	ID              primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
 	NamaLengkap     string             `bson:"namalengkap,omitempty" json:"namalengkap,omitempty"`
 	Email           string             `bson:"email,omitempty" json:"email,omitempty"`
-	Password        string             `bson:"password,omitempty" json:"password,omitempty"`
-	Confirmpassword string             `bson:"confirmpass,omitempty" json:"confirmpass,omitempty"`
-	Salt            string             `bson:"salt,omitempty" json:"salt,omitempty"`
+	// Password        string             `bson:"password,omitempty" json:"password,omitempty"`
+	// Confirmpassword string             `bson:"confirmpass,omitempty" json:"confirmpass,omitempty"`
+	// Salt            string             `bson:"salt,omitempty" json:"salt,omitempty"`
 }
 
-type Password struct {
-	Password        string `bson:"password,omitempty" json:"password,omitempty"`
-	Newpassword     string `bson:"newpass,omitempty" json:"newpass,omitempty"`
-	Confirmpassword string `bson:"confirmpass,omitempty" json:"confirmpass,omitempty"`
-}
+// type Password struct {
+// 	Password        string `bson:"password,omitempty" json:"password,omitempty"`
+// 	Newpassword     string `bson:"newpass,omitempty" json:"newpass,omitempty"`
+// 	Confirmpassword string `bson:"confirmpass,omitempty" json:"confirmpass,omitempty"`
+// }
 
 type AIRequest struct {
 	ID              primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
