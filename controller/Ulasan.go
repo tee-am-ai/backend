5025e491921143dac01372e9d93560e5881f1446
Fatwaff <fax.mp4@gmail.com>
2024-07-21 09:26:54 +0700

    Add Ulasan controller

---
+++ controller/Ulasan.go
@@ -0,0 +1,15 @@
+package controller
+
+import (
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// Fungsi untuk menambahkan ulasan
+func AddUlasan(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
+	
+	
+}
+
+
