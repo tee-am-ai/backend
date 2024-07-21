f056490a5dcd2952d241abd538dd25be6a6a7e86
Fatwaff <fax.mp4@gmail.com>
2024-07-21 09:27:17 +0700

    Add model package import and declare ulasan variable

--- controller/Ulasan.go
+++ controller/Ulasan.go
@@ -3,13 +3,15 @@ package controller
 import (
 	"net/http"
 
+	model "github.com/tee-am-ai/backend/model"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Fungsi untuk menambahkan ulasan
 func AddUlasan(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
+	var ulasan model.Ulasan
+
 	
 	
 }
-
 
