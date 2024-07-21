8269939cd24da621e4c5414f00db03dfc655278c
Fatwaff <fax.mp4@gmail.com>
2024-07-21 09:19:59 +0700

    Fix import statement in GetEnv function

--- config/config.go
+++ config/config.go
@@ -1,13 +1,13 @@
 package config
 
 import (
-	"os"
+	// "os"
 
 	"github.com/joho/godotenv"
 )
 
 func GetEnv(envName string) string {
 	envFile, _ := godotenv.Read("../.env")
-	// return envFile[envName]
-	return os.Getenv(envName)
+	return envFile[envName]
+	// return os.Getenv(envName)
 }
