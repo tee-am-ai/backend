8b5694129e89bccb3aa231317e10340a82fb5f86
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-21 21:50:17 +0700

    Refactor type.go to update struct definitions

--- model/type.go
+++ model/type.go
@@ -50,7 +50,7 @@ type Response struct {
 type Payload struct {
 	Id    primitive.ObjectID `json:"id"`
 	Email string             `json:"email"`
-	// Exp   time.Time          `json:"exp"`
-	// Iat   time.Time          `json:"iat"`
-	// Nbf   time.Time          `json:"nbf"`
+	Exp   time.Time          `json:"exp"`
+	Iat   time.Time          `json:"iat"`
+	Nbf   time.Time          `json:"nbf"`
 }
