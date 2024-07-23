c29cb14fddc4ad118887f41f1563a18c3e2ad220
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-23 09:44:58 +0700

    Refactor Login.go for improved password validation and authentication

--- controller/Login.go
+++ controller/Login.go
@@ -28,7 +28,16 @@ func LogIn(db *mongo.Database, respw http.ResponseWriter, req *http.Request, pri
 		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "email tidak valid")
 		return
 	}
-
+	existsDoc, err := helper.GetUserFromEmail(user.Email, db)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : get email " + err.Error())
+		return
+	}
+	salt, err := hex.DecodeString(existsDoc.Salt)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : salt")
+		return
+	}
 	hash := argon2.IDKey([]byte(user.Password), salt, 1, 64*1024, 4, 32)
 	if hex.EncodeToString(hash) != existsDoc.Password {
 		helper.ErrorResponse(respw, req, http.StatusUnauthorized, "Unauthorized", "password salah")
