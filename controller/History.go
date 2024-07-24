de58aa79d1bcdb88175f06f852962057894f7505
Fatwaff <fax.mp4@gmail.com>
2024-07-24 13:51:03 +0700

    Add History function with MongoDB integration

--- controller/History.go
+++ controller/History.go
@@ -1,5 +1,11 @@
 package controller
 
-func History() {
+import (
+	"net/http"
 
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func History(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
+	
 }
