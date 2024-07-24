6ea9532f819ccac40650c7aba0ff072420f9de24
Fatwaff <fax.mp4@gmail.com>
2024-07-24 13:57:06 +0700

    Add GetUserLogin function to handle user login

--- helper/mongo.go
+++ helper/mongo.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net/http"
 
 	"github.com/tee-am-ai/backend/model"
 	"go.mongodb.org/mongo-driver/bson"
@@ -72,4 +73,13 @@ func GetUserFromID(_id primitive.ObjectID, db *mongo.Database) (doc model.User, 
 		return doc, fmt.Errorf("error retrieving data for ID %s: %s", _id, err.Error())
 	}
 	return doc, nil
+}
+
+func GetUserLogin(PASETOPUBLICKEYENV string, r *http.Request) (Payload, error) {
+	tokenstring := r.Header.Get("Authorization")
+	payload, err := Decode(PASETOPUBLICKEYENV, tokenstring)
+	if err != nil {
+		return payload, err
+	}
+	return payload, nil
 }
