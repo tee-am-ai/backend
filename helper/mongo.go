61160ece89328e33f3e74555075c4954463f84f7
aulianafahrian <aulianafahrian@gmail.com>
2024-07-26 23:13:39 +0700

    refactor: Remove unused code in mongo.go

--- helper/mongo.go
+++ helper/mongo.go
@@ -2,10 +2,7 @@ package helper
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/tee-am-ai/backend/model"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -30,17 +27,4 @@ func InsertOneDoc(db *mongo.Database, col string, doc any) (insertedID primitive
 		return
 	}
 	return result.InsertedID.(primitive.ObjectID), nil
-}
-
-func GetUserFromEmail(email string, db *mongo.Database) (doc model.User, err error) {
-	collection := db.Collection("users")
-	filter := bson.M{"email": email}
-	err = collection.FindOne(context.TODO(), filter).Decode(&doc)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return doc, fmt.Errorf("email tidak ditemukan")
-		}
-		return doc, fmt.Errorf("kesalahan server")
-	}
-	return doc, nil
 }
