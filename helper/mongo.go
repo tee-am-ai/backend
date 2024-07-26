91ab2c60588e6a1b24d9a5b4557711869ee79eb8
aulianafahrian <aulianafahrian@gmail.com>
2024-07-26 23:14:29 +0700

    refactor: Remove unused code in mongo.go

--- helper/mongo.go
+++ helper/mongo.go
@@ -1,21 +1,6 @@
 package helper
 
-import (
-	"context"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-)
-
 type DBInfo struct {
 	DBString string
 	DBName   string
-}
-
-func MongoConnect(mconn DBInfo) (db *mongo.Database, err error) {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mconn.DBString))
-	if err != nil {
-		return nil, err
-	}
-	return client.Database(mconn.DBName), nil
 }
