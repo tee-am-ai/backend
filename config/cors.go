bb9ea3ccebfd54f4758507e7a85efa263f093e62
aulianafahrian <aulianafahrian@gmail.com>
2024-07-26 22:52:41 +0700

    chore: Update CORS handling and SetAccessControlHeaders function

--- config/cors.go
+++ config/cors.go
@@ -1,5 +1,7 @@
 package config
 
+import "net/http"
+
 // Daftar origins yang diizinkan
 var Origins = []string{
 	"http://localhost:5173",
@@ -11,4 +13,38 @@ var Origins = []string{
 	"http://127.0.0.1:5173",
 	"https://tee-am-ai.github.io",
 	"https://tee-am-ai.vercel.app",
+}
+
+// Fungsi untuk memeriksa apakah origin diizinkan
+func isAllowedOrigin(origin string) bool {
+	for _, o := range Origins {
+		if o == origin {
+			return true
+		}
+	}
+	return false
+}
+
+// Fungsi untuk mengatur header CORS
+func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
+	origin := r.Header.Get("Origin")
+
+	if isAllowedOrigin(origin) {
+		// Set CORS headers for the preflight request
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
+			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Max-Age", "3600")
+			w.WriteHeader(http.StatusNoContent)
+			return true
+		}
+		// Set CORS headers for the main request.
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		return false
+	}
+
+	return false
 }
