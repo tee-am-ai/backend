76179207dbc1eccc26a2453cd20f84d51e44af26
aulianafahrian <aulianafahrian@gmail.com>
2024-07-26 22:50:08 +0700

    chore: Update GetEnv function to use os.Getenv for reading environment variables

--- config/config.go
+++ config/config.go
@@ -2,11 +2,8 @@ package config
 
 import (
 	"os"
-	// "github.com/joho/godotenv"
 )
 
 func GetEnv(envName string) string {
-	// envFile, _ := godotenv.Read("../.env")
-	// return envFile[envName]
 	return os.Getenv(envName)
 }
