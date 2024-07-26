c810c910e88127e062978d633585814eee6e3900
aulianafahrian <aulianafahrian@gmail.com>
2024-07-26 23:13:18 +0700

    feat: Refactor MongoDB connection function for improved error handling and readability

--- helper/mongo.go
+++ helper/mongo.go
@@ -44,18 +44,3 @@ func GetUserFromEmail(email string, db *mongo.Database) (doc model.User, err err
 	}
 	return doc, nil
 }
-
-func GetAllDocs[T any](db *mongo.Database, col string, filter bson.M) (docs T, err error) {
-	ctx := context.TODO()
-	collection := db.Collection(col)
-	cursor, err := collection.Find(ctx, filter)
-	if err != nil {
-		return
-	}
-	defer cursor.Close(ctx)
-	err = cursor.All(context.TODO(), &docs)
-	if err != nil {
-		return
-	}
-	return
-}
