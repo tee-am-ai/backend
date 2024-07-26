069b7420d82d11b0237958b7d7202d3b40e47d2f
aulianafahrian <aulianafahrian@gmail.com>
2024-07-26 22:48:25 +0700

    chore: Refactor GetEnv function to read environment variables from .env file

--- config/config.go
+++ config/config.go
@@ -1,13 +1,10 @@
 package config
 
 import (
-	"os"
-
 	"github.com/joho/godotenv"
 )
 
 func GetEnv(envName string) string {
 	envFile, _ := godotenv.Read("../.env")
 	return envFile[envName]
-	return os.Getenv(envName)
 }
