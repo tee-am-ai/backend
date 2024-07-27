666bbc1032600bb8bd9297a66b769f26fe281891
aulianafahrian <aulianafahrian@gmail.com>
2024-07-27 22:32:19 +0700

    refactor: Update AddUlasan function in Ulasan controller and improve error handling

--- controller/Ulasan.go
+++ controller/Ulasan.go
@@ -1,12 +1,23 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/tee-am-ai/backend/helper"
+	model "github.com/tee-am-ai/backend/model"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Fungsi untuk menambahkan ulasan
 func AddUlasan(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
+	var ulasan model.Ulasan
+
+	// Decode request body menjadi struct Ulasan
+	err := json.NewDecoder(req.Body).Decode(&ulasan)
+	if err != nil {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
+		return
+	}
 
 }
