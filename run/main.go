adc3f89c4e3c48209f78daecfd171a65b80dbb9e
aulianafahrian <aulianafahrian@gmail.com>
2024-07-27 22:22:10 +0700

    refactor: Remove unused code in main.go and improve error handling in MongoDB connection function

---
+++ run/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/tee-am-ai/backend/routes"
+)
+
+func main() {
+	http.HandleFunc("/", routes.URL)
+	port := ":8080"
+	fmt.Println("Server started at: http://localhost" + port)
+	http.ListenAndServe(port, nil)
+}
