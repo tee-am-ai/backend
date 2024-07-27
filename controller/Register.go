b0756f704ef04f63baafe0d08d0a46db83beb560
aulianafahrian <aulianafahrian@gmail.com>
2024-07-27 22:26:41 +0700

    refactor: Improve error handling and remove unused code in SignUp function

--- controller/Register.go
+++ controller/Register.go
@@ -19,4 +19,9 @@ func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http
 		return
 	}
 
+	if user.NamaLengkap == "" || user.Email == "" || user.Password == "" || user.Confirmpassword == "" {
+		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "mohon untuk melengkapi data user")
+		return
+	}
+
 }
