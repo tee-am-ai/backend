18dab9c9ef52a4fdacbc990a38f343779312ccf9
aulianafahrian <aulianafahrian@gmail.com>
2024-07-27 22:39:07 +0700

    refactor: Remove unused UpdateOneDoc function in mongo.go

--- helper/mongo.go
+++ helper/mongo.go
@@ -2,9 +2,7 @@ package helper
 
 import (
 	"context"
-	"fmt"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -29,17 +27,4 @@ func InsertOneDoc(db *mongo.Database, col string, doc any) (insertedID primitive
 		return
 	}
 	return result.InsertedID.(primitive.ObjectID), nil
-}
-
-func UpdateOneDoc(db *mongo.Database, col string, id primitive.ObjectID, doc any) (err error) {
-	filter := bson.M{"_id": id}
-	result, err := db.Collection(col).UpdateOne(context.Background(), filter, bson.M{"$set": doc})
-	if err != nil {
-		return fmt.Errorf("error update: %v", err)
-	}
-	if result.ModifiedCount == 0 {
-		err = fmt.Errorf("tidak ada data yang diubah")
-		return
-	}
-	return nil
 }
