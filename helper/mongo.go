0f4124f38a5d1f6290566c8b35efeb66be4308f5
aulianafahrian <aulianafahrian@gmail.com>
2024-07-27 22:40:09 +0700

    refactor: Remove unused InsertOneDoc function and improve error handling in mongo.go

--- helper/mongo.go
+++ helper/mongo.go
@@ -3,6 +3,7 @@ package helper
 import (
 	"context"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -18,4 +19,12 @@ func MongoConnect(mconn DBInfo) (db *mongo.Database, err error) {
 		return nil, err
 	}
 	return client.Database(mconn.DBName), nil
+}
+
+func InsertOneDoc(db *mongo.Database, col string, doc any) (insertedID primitive.ObjectID, err error) {
+	result, err := db.Collection(col).InsertOne(context.Background(), doc)
+	if err != nil {
+		return
+	}
+	return result.InsertedID.(primitive.ObjectID), nil
 }
