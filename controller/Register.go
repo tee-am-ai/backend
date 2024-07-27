031047be3a15d97da08f2d595586de465096ccbf
aulianafahrian <aulianafahrian@gmail.com>
2024-07-27 22:25:56 +0700

    refactor: Remove unused code and improve error handling in SignUp function

--- controller/Register.go
+++ controller/Register.go
@@ -1,10 +1,8 @@
 package controller
 
 import (
-	"encoding/json"
 	"net/http"
 
-	"github.com/tee-am-ai/backend/helper"
 	model "github.com/tee-am-ai/backend/model"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -12,11 +10,5 @@ import (
 // SignUp
 func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http.Request) {
 	var user model.User
-
-	err := json.NewDecoder(req.Body).Decode(&user)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusBadRequest, "Bad Request", "error parsing request body "+err.Error())
-		return
-	}
 
 }
