68409fe232bc9f4034272045901b5fe30307e694
aulianafahrian <aulianafahrian@gmail.com>
2024-07-27 22:23:46 +0700

    refactor: Remove unused code in Register.go and improve error handling in SignUp function

--- controller/Register.go
+++ controller/Register.go
@@ -59,17 +59,5 @@ func SignUp(db *mongo.Database, col string, respw http.ResponseWriter, req *http
 		"password":    hex.EncodeToString(hashedPassword),
 		"salt":        hex.EncodeToString(salt),
 	}
-	insertedID, err := helper.InsertOneDoc(db, col, userData)
-	if err != nil {
-		helper.ErrorResponse(respw, req, http.StatusInternalServerError, "Internal Server Error", "kesalahan server : insert data, "+err.Error())
-		return
-	}
-	resp := map[string]any{
-		"message":    "berhasil mendaftar",
-		"insertedID": insertedID,
-		"data": map[string]string{
-			"email": user.Email,
-		},
-	}
-	helper.WriteJSON(respw, http.StatusCreated, resp)
+
 }
