26fe8d81b0cf8471582e4b42bdce5639e790fc1f
aulianafahrian <aulianafahrian@gmail.com>
2024-07-27 22:44:11 +0700

    refactor: Remove unused Encode function and update error handling in paseto.go

--- helper/paseto.go
+++ helper/paseto.go
@@ -3,7 +3,6 @@ package helper
 import (
 	"time"
 
-	"aidanwoods.dev/go-paseto"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -13,15 +12,4 @@ type Payload struct {
 	Exp   time.Time          `json:"exp"`
 	Iat   time.Time          `json:"iat"`
 	Nbf   time.Time          `json:"nbf"`
-}
-
-func Encode(id primitive.ObjectID, email, privateKey string) (string, error) {
-	token := paseto.NewToken()
-	token.SetIssuedAt(time.Now())
-	token.SetNotBefore(time.Now())
-	token.SetExpiration(time.Now().Add(2 * time.Hour))
-	token.Set("id", id)
-	token.SetString("email", email)
-	secretKey, err := paseto.NewV4AsymmetricSecretKeyFromHex(privateKey)
-	return token.V4Sign(secretKey, nil), err
 }
